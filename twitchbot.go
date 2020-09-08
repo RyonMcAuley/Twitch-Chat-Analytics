311ce3451759144e77d72343a6f442ed588533f9
RyonMcAuley <rdmcauley@gmail.com>
2020-09-08 16:34:24 -0400

    added structs for a basic bot & auth credentials

--- twitchbot.go
+++ twitchbot.go
@@ -1,5 +1,10 @@
 package twitchbot
 
+import (
+	"net"
+	"time"
+)
+
 //TwitchBot interface for accessing chat
 type TwitchBot interface {
 	//Connects to the twitch chat server
@@ -12,4 +17,41 @@ type TwitchBot interface {
 	HandleChat() error
 	//Keeps bot connected and handling chat
 	Start()
+}
+
+//OAuthCred credentials
+type OAuthCred struct {
+	Password string `json:"password,omitempty"`
+}
+
+//BasicBot struct object that does the interacting
+// with the chat
+type BasicBot struct {
+	//Name of the channel to join
+	Channel string
+
+	//Path to private json auth token file
+	PrivatePath string
+
+	//Reference to bot's network connection
+	conn net.Conn
+
+	// The credentials necessary for authentication.
+	Credentials *OAuthCred
+
+	//forced delay between messages to avoid breaking twitch guidelines
+	MsgRate time.Duration
+
+	//Name for the bot to use in chat
+	Name string
+
+	//time that the bot is starting
+	// used for logging
+	startTime time.Time
+
+	//server domain of twitch chat server
+	Server string
+
+	// port for twitch chat server
+	Port string
 }
