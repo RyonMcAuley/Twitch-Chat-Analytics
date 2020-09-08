1bd4438dbd60a06e1f9c4f827aaab062bdb7a195
RyonMcAuley <rdmcauley@gmail.com>
2020-09-08 16:02:49 -0400

    added basic bot interface

--- twitchbot.go
+++ twitchbot.go
@@ -1,1 +1,15 @@
 package twitchbot
+
+//TwitchBot interface for accessing chat
+type TwitchBot interface {
+	//Connects to the twitch chat server
+	Connect()
+	//Disconnects from the twitch chat server
+	Disconnect()
+	//Joins a channel's chat in order to access it
+	JoinChannel()
+	//Listens to chat & maintains connection
+	HandleChat() error
+	//Keeps bot connected and handling chat
+	Start()
+}
